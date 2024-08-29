729711179116b1aa319ea85ff8de50e405921048
Nick Nielsen <70209373+nicknielsen@users.noreply.github.com>
2024-08-29 16:59:18 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hey there...
+          Hey, good looking!
         </p>
         <a
           className="App-link"
